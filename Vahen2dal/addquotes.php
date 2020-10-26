95fc6e3ff23a2118d8f8b2628c76b60b502edde8
Anna-Stiina Laidna <annalaid@users.noreplu.github.com>
2020-10-26 10:13:11 +0200

    vahenädal

---
+++ Vahen2dal/addquotes.php
@@ -0,0 +1,52 @@
+<?php
+require("session.php");
+require("header.php");
+require("../../../config.php");
+require("fnc_film.php");
+
+$notice = "";
+$selectedrole = "";
+$quote = "";
+
+if(isset($_POST["quotesubmit"])) {
+  if(!empty($_POST["quoteinput"])){
+  $quote = test_input($_POST["quoteinput"]);
+} else {
+  $notice .= " Sisesta tsitaat!";
+}
+if(!empty($_POST["roleinput"])){
+  $selectedrole = intval($_POST["roleinput"]);
+} else {
+  $notice .= " Vali seos filmiga!";
+}
+if(!empty($quote) and !empty($selectedrole)){
+  $notice = storenewquote($selectedrole, $quote);
+}
+}
+
+$roleselect = readroletoselect($selectedrole);
+?>
+
+<img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse banner">
+<h1><?php echo $_SESSION["userfirstname"] ." " .$_SESSION["userlastname"]; ?> programmeerib veebi</h1>
+<p>See veebileht on loodud õppetöö käigus ning ei sisada mingit tõsiseltvõetavat sisu!</p>
+<p>Leht on loodud veebiprogrammeerimise kursusel <a href="http://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
+<ul>
+<p><a href="?logout=1">Logi välja!</a></p>
+<hr>
+<li><a href="home.php">Avalehele</a></li>
+</ul>
+
+<h2>Tsitaadi lisamine</h2>
+<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<textarea rows="1" cols="39" name="quoteinput" id="quoteinput" placeholder="Tsitaat"><?php echo $quote; ?></textarea>
+<br />
+<?php echo $roleselect; ?>
+<br />
+<span><?php echo $notice; ?></span>
+<br />
+<input type="submit" name="quotesubmit" value="Salvesta tsitaat">
+</form>
+
+</body>
+</html>
---
+++ Vahen2dal/home.php
@@ -0,0 +1,29 @@
+<?php
+require("session.php");
+require("header.php");
+
+?>
+
+
+  <img src="../img/vp_banner.png" alt="Veebiprogrammeerimise kursuse banner">
+	<h1><?php echo $_SESSION["userfirstname"] ." " .$_SESSION["userlastname"]; ?> programmeerib veebi</h1>
+  <p>See veebileht on loodud õppetöö käigus ning ei sisada mingit tõsiseltvõetavat sisu!</p>
+  <p>Leht on loodud veebiprogrammeerimise kursusel <a href="http://www.tlu.ee">Tallinna Ülikooli</a> Digitehnoloogiate instituudis.</p>
+	<p><a href="?logout=1">Logi välja!</a></p>
+		<ul>
+     <li><a href = "nonsens.php">Sisesta oma mõtetu mõte!</a></li>
+     <li><a href="answers.php">Siit saad vaadata oma sisestatud mõtetuid mõtteid!</a></li>
+     <hr>
+     <li><a href="addfilms.php">Filmiinfo lisamine</a></li>
+		 <li><a href="listfilms.php">Filmiinfo näitamine</a></li>
+     <hr>
+     <li><a href="addquotes.php">Tsitaatide lisamine</a></li>
+		 <li><a href="addfilmrelations.php">Filmide seoste loomine</a></li>
+		 <li><a href="listfilmrelations.php">Filmide nimekiri</a></li>
+     <hr>
+		 <li><a href="userprofile.php">Kasutaja profiili haldamine</a></li>
+    </ul>
+	<br>
+
+</body>
+</html>
